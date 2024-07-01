1990b72d90bc2f20d123ef82b2b338e9df0ef15d
Andrei Avramenko <aladser@gmail.com>
2024-07-01 19:44:35 +0900

    обновлен интерфейс

--- FNS_MFC/MainForm.Designer.cs
+++ FNS_MFC/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.selectButton = new System.Windows.Forms.Button();
             this.runButton = new System.Windows.Forms.Button();
@@ -36,16 +37,19 @@
             this.unitLabel = new System.Windows.Forms.Label();
             this.unitComboBox = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.newNameField = new System.Windows.Forms.Label();
             this.oldNameFiled = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // selectButton
             // 
-            this.selectButton.Location = new System.Drawing.Point(17, 15);
+            this.selectButton.Location = new System.Drawing.Point(23, 18);
+            this.selectButton.Margin = new System.Windows.Forms.Padding(4);
             this.selectButton.Name = "selectButton";
-            this.selectButton.Size = new System.Drawing.Size(137, 59);
+            this.selectButton.Size = new System.Drawing.Size(183, 73);
             this.selectButton.TabIndex = 0;
             this.selectButton.Text = "Открыть файл";
             this.selectButton.UseVisualStyleBackColor = true;
@@ -53,9 +57,10 @@
             // 
             // runButton
             // 
-            this.runButton.Location = new System.Drawing.Point(17, 164);
+            this.runButton.Location = new System.Drawing.Point(23, 202);
+            this.runButton.Margin = new System.Windows.Forms.Padding(4);
             this.runButton.Name = "runButton";
-            this.runButton.Size = new System.Drawing.Size(137, 59);
+            this.runButton.Size = new System.Drawing.Size(183, 73);
             this.runButton.TabIndex = 1;
             this.runButton.Text = "Выполнить";
             this.runButton.UseVisualStyleBackColor = true;
@@ -65,10 +70,10 @@
             // 
             this.oldNameLabel.BackColor = System.Drawing.SystemColors.Control;
             this.oldNameLabel.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.oldNameLabel.Location = new System.Drawing.Point(173, 38);
-            this.oldNameLabel.Margin = new System.Windows.Forms.Padding(3);
+            this.oldNameLabel.Location = new System.Drawing.Point(231, 47);
+            this.oldNameLabel.Margin = new System.Windows.Forms.Padding(4);
             this.oldNameLabel.Name = "oldNameLabel";
-            this.oldNameLabel.Size = new System.Drawing.Size(89, 23);
+            this.oldNameLabel.Size = new System.Drawing.Size(119, 28);
             this.oldNameLabel.TabIndex = 4;
             this.oldNameLabel.Text = "Открыт файл";
             this.oldNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -76,9 +81,10 @@
             // newNameLabel
             // 
             this.newNameLabel.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.newNameLabel.Location = new System.Drawing.Point(173, 164);
+            this.newNameLabel.Location = new System.Drawing.Point(231, 202);
+            this.newNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.newNameLabel.Name = "newNameLabel";
-            this.newNameLabel.Size = new System.Drawing.Size(89, 59);
+            this.newNameLabel.Size = new System.Drawing.Size(119, 73);
             this.newNameLabel.TabIndex = 5;
             this.newNameLabel.Text = "Новый файл сохранен как";
             this.newNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -86,9 +92,10 @@
             // unitLabel
             // 
             this.unitLabel.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.unitLabel.Location = new System.Drawing.Point(173, 101);
+            this.unitLabel.Location = new System.Drawing.Point(231, 124);
+            this.unitLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.unitLabel.Name = "unitLabel";
-            this.unitLabel.Size = new System.Drawing.Size(89, 34);
+            this.unitLabel.Size = new System.Drawing.Size(119, 42);
             this.unitLabel.TabIndex = 6;
             this.unitLabel.Text = "Отделение:";
             this.unitLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -124,15 +131,17 @@
             "Февральск",
             "Циолковский",
             "Шимановск"});
-            this.unitComboBox.Location = new System.Drawing.Point(271, 104);
+            this.unitComboBox.Location = new System.Drawing.Point(361, 128);
+            this.unitComboBox.Margin = new System.Windows.Forms.Padding(4);
             this.unitComboBox.Name = "unitComboBox";
-            this.unitComboBox.Size = new System.Drawing.Size(156, 28);
+            this.unitComboBox.Size = new System.Drawing.Size(207, 32);
             this.unitComboBox.TabIndex = 7;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.Control;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.newNameField);
             this.panel1.Controls.Add(this.oldNameFiled);
             this.panel1.Controls.Add(this.selectButton);
@@ -141,40 +150,58 @@
             this.panel1.Controls.Add(this.newNameLabel);
             this.panel1.Controls.Add(this.runButton);
             this.panel1.Controls.Add(this.oldNameLabel);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(614, 249);
+            this.panel1.Size = new System.Drawing.Size(818, 306);
             this.panel1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.Image = global::FNS_MFC.Properties.Resources.info;
+            this.label1.Location = new System.Drawing.Point(760, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 32);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "                   ";
+            this.toolTip1.SetToolTip(this.label1, "Программа подготовки документов для ФНС 1.2\r\nAladser (c)\r\n2024");
             // 
             // newNameField
             // 
             this.newNameField.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.newNameField.Location = new System.Drawing.Point(271, 164);
+            this.newNameField.Location = new System.Drawing.Point(361, 202);
+            this.newNameField.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.newNameField.Name = "newNameField";
-            this.newNameField.Padding = new System.Windows.Forms.Padding(5);
-            this.newNameField.Size = new System.Drawing.Size(328, 59);
+            this.newNameField.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.newNameField.Size = new System.Drawing.Size(437, 73);
             this.newNameField.TabIndex = 9;
             // 
             // oldNameFiled
             // 
             this.oldNameFiled.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.oldNameFiled.Location = new System.Drawing.Point(268, 15);
+            this.oldNameFiled.Location = new System.Drawing.Point(357, 18);
+            this.oldNameFiled.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.oldNameFiled.Name = "oldNameFiled";
-            this.oldNameFiled.Padding = new System.Windows.Forms.Padding(5);
-            this.oldNameFiled.Size = new System.Drawing.Size(331, 59);
+            this.oldNameFiled.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.oldNameFiled.Size = new System.Drawing.Size(441, 73);
             this.oldNameFiled.TabIndex = 8;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup_1);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(642, 276);
+            this.ClientSize = new System.Drawing.Size(856, 340);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
-            this.Text = "ФНС 1.01";
+            this.Text = "ФНС 1.2";
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -191,6 +218,8 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label newNameField;
         private System.Windows.Forms.Label oldNameFiled;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
